d40a1228730431d878a86924d9d72b172e992058
Jan Ruhfus <jankrb@jankrb.com>
2021-05-17 12:08:46 +0200

    Impl tailwind in vuejs & postcss

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,51 @@
 require("./bootstrap");
 
 import Vue from 'vue';
+import VueTailwind from 'vue-tailwind'
+
+import {
+    TInput,
+    TTextarea,
+    TSelect,
+    TRadio,
+    TCheckbox,
+    TButton,
+    TInputGroup,
+    TCard,
+    TAlert,
+    TModal,
+    TDropdown,
+    TRichSelect,
+    TPagination,
+    TTag,
+    TRadioGroup,
+    TCheckboxGroup,
+    TTable,
+    TDatepicker,
+    TToggle,
+    TDialog,
+} from 'vue-tailwind/dist/components';
+
 window.Vue = Vue;
 
 Vue.component("kanban-board", require("./components/KanbanBoard.vue").default);
+
+const settings = {
+    't-input': {
+        component: TInput,
+        props: {
+            classes: 'border-2 block w-full rounded text-gray-800'
+        }
+    },
+    't-textarea': {
+        component: TTextarea,
+        props: {
+            classes: 'border-2 block w-full rounded text-gray-800'
+        }
+    },
+}
+
+Vue.use(VueTailwind, settings)
 
 const app = new Vue({
     el: "#app"
